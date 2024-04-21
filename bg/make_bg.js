c9c98920172dfb584185173ef744b6904e1c0c3d
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-04-21 14:59:06 +1000

    Finalised some stuff with the bg!

--- bg/make_bg.js
+++ bg/make_bg.js
@@ -1,3 +1,8 @@
+const MAX_SIZE = 100;
+const MAX_INVIS_SIZE = 10;
+const MAX_LINES = 20;
+
+
 function make_line(padding=0) {
     document.getElementById('code-blocks').innerHTML += '<div class="code-line" style="padding-left: '+padding.toString()+'px"></div>';
 }
@@ -15,6 +20,10 @@ function get_size(html_block) {
     return parseInt(html_block.match('(?<=style="width: ).+?(?=px)')[0]);
 }
 
+function is_invisible(html_block) {
+    return html_block.includes('invisible');
+}
+
 function get_padding(html_block) {
     return parseInt(html_block.match('(?<=style="padding-left: ).+?(?=px)')[0]);
 }
@@ -28,37 +37,39 @@ function total_length(line) {
     return sum;
 }
 
-const max_size = 100;
-
 function next() {
     var lines = document.getElementById('code-blocks').children;
     var currentline = lines[lines.length-1];
     var make_newline = false;
     var padding = get_padding(currentline.outerHTML);
     var linetotallen = total_length(currentline);
-    if (linetotallen > document.body.offsetWidth-40-padding-80) {
+    if (linetotallen > document.body.offsetWidth-120-padding) {
         make_newline = true;
     }
     if ((make_newline || currentline.children.length > 10 || Math.random() < 0.2)&&linetotallen > 50) {
         make_newline = true;
-        if (lines.length > 10) {
+        if (lines.length > MAX_LINES) {
             lines[0].remove();
         }
         var changeby = 0;
         var result = Math.random();
         if (result < 0.35) {
             changeby = 1;
-        } else if (result > 0.65) {
+        } else if (result > 0.6) {
             changeby = -1;
         }
         make_line(Math.max(0, Math.min(padding+changeby*40, 360)));
     } else {make_newline = false;}
-    if (make_newline || currentline.children.length == 0 || get_size(currentline.children[currentline.children.length-1].outerHTML) > max_size || Math.random() < 0.1) {
+    if (make_newline || currentline.children.length == 0 || get_size(currentline.children[currentline.children.length-1].outerHTML) > MAX_SIZE || Math.random() < 0.1) {
         make_block(Math.random()*20 + Math.random()*20, Math.random()<0.2&&!make_newline);
     } else {
         var currentblock = currentline.children[currentline.children.length-1]
         var size = get_size(currentblock.outerHTML);
-        currentblock.outerHTML = currentblock.outerHTML.replace(size.toString(), Math.min(210, Math.round(size+Math.random()*(max_size/4)+max_size/4)).toString())
+        var max_this_size = MAX_SIZE;
+        if (is_invisible(currentblock.outerHTML)) {
+            max_this_size = MAX_INVIS_SIZE;
+        }
+        currentblock.outerHTML = currentblock.outerHTML.replace(size.toString(), Math.min(max_this_size+(max_this_size/10), Math.round(size+Math.random()*(MAX_SIZE/4)+max_this_size/4)).toString())
     }
     //var currentblock = currentline.children[currentline.children.length-1]
     //currentblock.innerHTML = get_size(currentblock.outerHTML);
